d5d775bd55b54110b37fabd976a017d2c0406129
nayara souza <nayaragsouza@live.com>
2019-10-17 19:49:04 -0300

    rails-mister-cocktails"

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.new
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.find(params[:id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
